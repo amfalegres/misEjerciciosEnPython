3093516b7018b796ebc473c2d350a8098172b014
fernando <fernandomaq2010@gmail.com>
2024-09-30 17:36:28 -0500

    mi primer commit.XD

---
+++ git.py
@@ -0,0 +1,14 @@
+# mis comandos en git
+
+# git init                inicia un nuevo repositorio/carpeta en git
+# git status              ver el estado de git
+
+# On branch master
+
+# No commits yet
+
+# Untracked files:
+#   (use "git add <file>..." to include in what will be committed)
+#         git.py
+
+# nothing added to commit but untracked files present (use "git add" to track)
