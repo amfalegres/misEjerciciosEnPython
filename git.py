929a909c6b6de44b2aab7c7120e2d4ee9ae1133a
fernando <fernandomaq2010@gmail.com>
2024-09-30 18:04:12 -0500

    ya se usar log

--- git.py
+++ git.py
@@ -16,4 +16,17 @@
 # git add .               para agregar archivos para que puedan ver los cambias
 
 #git commit -m "acá ba el mensaje"       para hacer commit
-#                       
+
+#git log      nos permite saber que hay dentro de todos los commit que hicimos
+
+# commit ecc209d3df4a6a455a1c3434567ae576f4c7f295 (HEAD -> master)
+# Author: fernando <fernandomaq2010@gmail.com>
+# Date:   Mon Sep 30 17:42:56 2024 -0500
+
+#     aprendi a agregar un commit :')
+
+# commit 3093516b7018b796ebc473c2d350a8098172b014
+# Author: fernando <fernandomaq2010@gmail.com>
+# Date:   Mon Sep 30 17:36:28 2024 -0500
+
+#     mi primer commit.XD
